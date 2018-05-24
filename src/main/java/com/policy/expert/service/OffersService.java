019b8b6e4f936b07f7668bab22691296f1aa7540
jan <jan.deulofeu@scalarmonitoring.com>
2018-05-24 22:16:14 +0100

    Adding offers service

---
+++ src/main/java/com/policy/expert/service/OffersService.java
@@ -0,0 +1,14 @@
+package com.policy.expert.service;
+
+import com.policy.expert.model.Offer;
+
+import java.util.List;
+
+public interface OffersService {
+
+    void addOffer(Offer offer);
+
+    void removeOffer(Offer offer);
+
+    List<Offer> listOffers();
+}
