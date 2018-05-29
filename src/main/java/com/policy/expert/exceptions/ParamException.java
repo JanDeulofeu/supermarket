6ddf65c3ede67c93a80e35dc9e7fd869c7fe7916
jan <jan.deulofeu@scalarmonitoring.com>
2018-05-29 06:48:33 +0100

    Removing unused method

--- src/main/java/com/policy/expert/exceptions/ParamException.java
+++ src/main/java/com/policy/expert/exceptions/ParamException.java
@@ -2,10 +2,6 @@ package com.policy.expert.exceptions;
 
 public class ParamException extends RuntimeException {
 
-    public ParamException(final String message) {
-        super(message);
-    }
-
     public ParamException(final String message, final Throwable throwable) {
         super(message, throwable);
     }
